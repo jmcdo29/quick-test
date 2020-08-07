c41924c9a888463170c580091fcd1565304a8b63
Jay Mcdoniel <jaymcd@amazon.com>
2020-08-07 08:56:06 -0700

    initial commit

---
+++ src/app.controller.spec.ts
@@ -0,0 +1,32 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+describe('AppController', () => {
+  let appController: AppController;
+
+  beforeAll(() => {
+    console.time(AppController.name);
+  });
+
+  afterAll(() => {
+    console.timeEnd(AppController.name);
+  });
+
+  beforeEach(async () => {
+    console.time(beforeEach.name);
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [AppController],
+      providers: [AppService],
+    }).compile();
+
+    appController = app.get<AppController>(AppController);
+    console.timeEnd(beforeEach.name);
+  });
+
+  describe('root', () => {
+    it('should return "Hello World!"', () => {
+      expect(appController.getHello()).toBe('Hello World!');
+    });
+  });
+});
